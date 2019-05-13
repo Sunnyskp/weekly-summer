11a4009d3e63efee32aec98d017abf4b2b528e5b
Panacci Rocco <panaccir@nj-00118-001.mediastudies.ca>
2019-05-13 15:49:53 -0400

    Finalize UI changes

--- week-02/instructor/tic-tac-toe/js/main.js
+++ week-02/instructor/tic-tac-toe/js/main.js
@@ -17,26 +17,43 @@ let user0 = {
 
 let whosTurnIsIt = `X`;
 
+// Store a reference
+const theBoard = document.querySelector('.board');
+
+
+
+const writeMsg = (msg = ``) => {
+  console.log(msg);
+  document.querySelector('.msg').innerHTML = msg;
+}
+
 // drawTheBoard(): redraw the board to our UI
 const drawTheBoard = (aBoard) => {
 
   // Copy the values to a new array, if null, create blank
-  const newBoard = aBoard.map(sq => {
-    if (!sq) return ` `;
+  const newBoard = aBoard.map((sq, i)=> {
+    if (!sq) return i;
     return sq;
   });
 
   console.log(`
- ${newBoard[0]} | ${newBoard[1]} | ${newBoard[2]} 
------------
- ${newBoard[3]} | ${newBoard[4]} | ${newBoard[5]}
------------
- ${newBoard[6]} | ${newBoard[7]} | ${newBoard[8]}
-`)
+    ${newBoard[0]} | ${newBoard[1]} | ${newBoard[2]} 
+    -----------
+    ${newBoard[3]} | ${newBoard[4]} | ${newBoard[5]}
+    -----------
+    ${newBoard[6]} | ${newBoard[7]} | ${newBoard[8]}
+  `)
+
+ 
+  theBoard.innerHTML = ``;  // Clear the board
+
   // Looping is another possibility
-  // aBoard.forEach(sq => {
-  //   if ()
-  // })
+  aBoard.forEach((sq, i) => {
+    if (!sq)
+      theBoard.innerHTML += `<div data-spot=${i}></div>` 
+    else
+      theBoard.innerHTML += `<div data-spot=${i}>${sq}</div>` 
+  });
 };
 
 // selectSquare(): returns true if the square is available, false otherwise
@@ -51,7 +68,7 @@ const selectSquare = (whoGoesThere, sqNum, aBoard) => {
     return true; // Good!
   }
   else {
-    console.log(`Spot already taken by: ${aBoard[sqNum]}`);
+    writeMsg(`Spot already taken by: ${aBoard[sqNum]}`);
     return false;  // Bad
   }
     
@@ -68,7 +85,7 @@ const changeUserTurn = (currTurn) => {
 }
 
 // checkBoard(): checks to see if anyone won (or if it's "cats game")
-const checkBoard = () => {
+const checkBoard = (aBoard) => {
   /* Winning scenarios:
       [0, 1, 2]
       [3, 4, 5]
@@ -78,27 +95,83 @@ const checkBoard = () => {
       [2, 5, 8]
       [0, 4, 8]
       [2, 4, 6]  */
+    
+  if ( 
+    ((aBoard[0] == aBoard[1]) && (aBoard[0] == aBoard[2]) && aBoard[0]) ||
+    ((aBoard[3] == aBoard[4]) && (aBoard[3] == aBoard[5]) && aBoard[3]) ||
+    ((aBoard[6] == aBoard[1]) && (aBoard[0] == aBoard[2]) && aBoard[6]) ||
+    ((aBoard[0] == aBoard[3]) && (aBoard[0] == aBoard[6]) && aBoard[0]) ||
+    ((aBoard[1] == aBoard[4]) && (aBoard[1] == aBoard[7]) && aBoard[1]) ||
+    ((aBoard[2] == aBoard[5]) && (aBoard[2] == aBoard[8]) && aBoard[2]) ||
+    ((aBoard[0] == aBoard[4]) && (aBoard[0] == aBoard[8]) && aBoard[0]) ||
+    ((aBoard[2] == aBoard[4]) && (aBoard[2] == aBoard[6]) && aBoard[2])
+  ) {
+    // Return true if winner
+    return true;
+  }
+
+  // Return false if no winner
+  return false;
 }
 
-const gameOver = () => {
 
+const gameOver = (whoWon) => {
+  writeMsg(`Game Over! ${whoWon} was the winner.`);
+  theBoard.innerHTML = "Winner!!"
+  return true;
 }
-
 
 // doTurn(): when a user selects a spot
-const doTurn = () => {
+const doTurn = (sq) => {
+  
+  if (!sq) {
+    writeMsg(`Woops, something went wrong. Try again.`);
+    return false;
+  }
+
   // 1. selectSquare
+  let wasValidMove = selectSquare(whosTurnIsIt, sq, board);
+
   // 2. checkBoard
     // If winner or cats: gameOver()
     // If no winner: changeUserTurn() and doTurn()
+  if ( wasValidMove ) {  // Valid move, board has already been redrawn
+    let isWinner = checkBoard(board);
+    if (isWinner) {
+      gameOver(whosTurnIsIt);
+    }
+    else {
+      whosTurnIsIt = changeUserTurn(whosTurnIsIt);
+      writeMsg(`It's now ${whosTurnIsIt}'s turn.`);
+      //doTurn();
+    }
+  }
+  else {
+    //doTurn();
+    drawTheBoard(board);
+    writeMsg(`Please go again.`);
+  }
+
 }
+
+
+theBoard.addEventListener(`click`, event => {
+  // Which spot did we click?
+  console.log("Something was clicked", event);
+
+  const whichSpot = event.target.dataset.spot;
+  if (!whichSpot) return;  // if no spot was stored, quit immediately!
+
+  doTurn(whichSpot);
+})
+
 
 
 
 
 // Start the application
 drawTheBoard(board);
-
+writeMsg(`It's now ${whosTurnIsIt}'s turn.`);
 
 // Simple test cases:
 // whosTurnIsIt = selectSquare(whosTurnIsIt, 4, board);
